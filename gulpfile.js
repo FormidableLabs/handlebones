d39afc7e7c7a813be1b03c20b28703c4ec4c441f
Ryan Eastridge <ryan@eastridge.me>
2014-03-10 15:43:58 -0700

    Better dist build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,40 @@
-var gulp = require('gulp'),
-  jshint = require('gulp-jshint');
+var gulp = require("gulp"),
+  jshint = require("gulp-jshint"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  clean = require("gulp-clean");
 
-gulp.task('lint', function() {
-  gulp.src('./handlebones.js')
+gulp.task("compress", function() {
+  return gulp.src("./handlebones.js")
+    .pipe(uglify({
+      outSourceMap: true
+    }))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("move-dist", ["compress"], function () {
+  return gulp.src("./dist/handlebones.js")
+    .pipe(rename("handlebones.min.js"))
+    .pipe(gulp.dest("."));
+});
+
+// TODO: probably a better way to combine
+
+gulp.task("move-map", ["move-dist"], function () {
+  return gulp.src("./dist/handlebones.js.map")
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("dist", ["move-map"], function () {
+  return gulp.src("./dist", {
+    read: false
+  }).pipe(clean({
+    force: true
+  }));
+});
+
+gulp.task("lint", function() {
+  return gulp.src("./handlebones.js")
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter("default"));
 });
